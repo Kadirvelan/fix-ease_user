046692f3b83bf1990be654f89f5335da9b18f82f
Kadirvelan <kadirvelanlinkes@gmail.com>
2021-11-22 00:09:40 +0530

    Map UI

--- lib/register_details.dart
+++ lib/register_details.dart
@@ -1,3 +1,5 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 
 class RegisterDetails extends StatefulWidget {
@@ -8,8 +10,69 @@ class RegisterDetails extends StatefulWidget {
 }
 
 class _RegisterDetailsState extends State<RegisterDetails> {
+  static const _initialCameraPosition =
+      CameraPosition(target: LatLng(13.010651, 80.2331943), zoom: 17);
+
+  late GoogleMapController _googleMapController;
+  late Position _position;
+
+  Future<Position> getUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: const [
+          GoogleMap(
+            initialCameraPosition: _initialCameraPosition,
+            zoomControlsEnabled: false,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+          ),
+        ],
+      ),
+    );
   }
 }
